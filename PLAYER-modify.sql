3cdeb5654966ad11e7ab0a8fbee63f97db9c3f3f
Gurjeevan Bains <gurjeevanb@gmail.com>
2016-05-26 15:42:32 -0700

    fixed player modify

--- PLAYER-modify.sql
+++ PLAYER-modify.sql
@@ -2,10 +2,3 @@ DELETE FROM Player
 WHERE 
   pos1 != 'QB' AND pos1 != 'RB' AND pos1 != 'WR' AND pos1 != 'TE';
 
-DELETE FROM Player
-WHERE
-  player NOT IN (SELECT DISTINCT trg FROM Pass 
-               UNION 
-               SELECT DISTINCT psr FROM Pass 
-               UNION 
-               SELECT DISTINCT bc FROM Rush);

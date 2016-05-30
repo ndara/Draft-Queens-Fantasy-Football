c801ce08f0f98bac25ee9472332edcdf25a27cb5
Gurjeevan Bains <gurjeevanb@gmail.com>
2016-05-29 17:34:44 -0700

    completed scoring for indvl players and entire teams

--- Scoring.java
+++ Scoring.java
@@ -14,58 +14,139 @@ public class Scoring {
       Connection conn = DBConnection.getConnection();
 
       //DB TESTING
-      getPlayerScore(conn, 1, "CN-0500", "QB");
-      //DBConnection.getAllPlayers(conn);
-      //DBConnection.updatePlayer(conn, "AA-1300", 42);
-      //DBConnection.resetPlayer(conn);
-      //DBConnection.getAllPlayers(conn);
-      //DBConnection.getAllTeamPlayers(conn, 42);
-      //DBConnection.getPosPlayers(conn, false, false, false, true, 1);
-      //DBConnection.initTeam(conn, "TESTING");
-      //DBConnection.updateTeam(conn, "RB1", "AA-1300", 3);
+      //getPlayerScore(conn,1 , "DW-4300");
+      DBConnection.initTeam(conn, "bitchnigga");
+      DBConnection.updateTeam(conn, "QB", "CN-0500", 1);
+      DBConnection.updateTeam(conn, "RB1", "DM-0450", 1);
+      DBConnection.updateTeam(conn, "RB2", "LB-0250", 1);
+      DBConnection.updateTeam(conn, "WR1", "WS-0925", 1);
+      DBConnection.updateTeam(conn, "WR2", "BM-0300", 1);
+      DBConnection.updateTeam(conn, "WR3", "JJ-4700", 1);
+      DBConnection.updateTeam(conn, "TE", "RG-2200", 1);
+      System.out.println("here is the total team score : " + getTeamScore(conn, 3, 1));
 
       DBConnection.close(conn);
    }
 
-   public static int getPlayerScore(Connection conn, int week, String player, String pos) {
-   	Statement statement = null;
+   public static double getTeamScore(Connection conn, int week, int id) {
+      Statement statement = null;
       ResultSet results = null;
-      int score = 0;
 
-      if (pos == "QB")
-         score = qbScore(conn, week, player);
-      //else if (pos == "RB")
-      	//score = rbScore(conn, week, player);
-      //else if (pos == "WR" || pos == "TE")
-      	//score = recScore(conn, week, player);
-      else
-      	System.out.println("Invalid Position type: " + pos);
+      String qbQuery = "SELECT QB FROM Team WHERE id = " + id;
+      String rb1Query = "SELECT RB1 FROM Team WHERE id = " + id;
+      String rb2Query = "SELECT RB2 FROM Team WHERE id = " + id;
+      String wr1Query = "SELECT WR1 FROM Team WHERE id = " + id;
+      String wr2Query = "SELECT WR2 FROM Team WHERE id = " + id;
+      String wr3Query = "SELECT WR3 FROM Team WHERE id = " + id;
+      String teQuery = "SELECT TE FROM Team WHERE id = " + id;
 
+      String qbStr = "";
+      String rb1Str = "";
+      String rb2Str = "";
+      String wr1Str = "";
+      String wr2Str = "";
+      String wr3Str = "";
+      String teStr = "";
+
+      double teamScore = 0;
+
+      try {
+         statement = conn.createStatement();
+         results = statement.executeQuery(qbQuery);
+         if(results.next())
+            qbStr = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(rb1Query);
+         if(results.next())
+            rb1Str = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(rb2Query);
+         if(results.next())
+            rb2Str = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(wr1Query);
+         if(results.next())
+            wr1Str = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(wr2Query);
+         if(results.next())
+            wr2Str = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(wr3Query);
+         if(results.next())
+            wr3Str = results.getString(1);
+
+         statement = conn.createStatement();
+         results = statement.executeQuery(teQuery);
+         if(results.next())
+            teStr = results.getString(1);
+
+
+         teamScore = getPlayerScore(conn, week, qbStr) + getPlayerScore(conn, week, rb1Str)
+                     + getPlayerScore(conn, week, rb2Str) + getPlayerScore(conn, week, wr1Str)
+                     + getPlayerScore(conn, week, wr2Str) + getPlayerScore(conn, week, wr3Str)
+                     + getPlayerScore(conn, week, teStr);
+
+
+
+      } catch (SQLException sqlEx) {
+         System.err.println("Error doing query: " + sqlEx);
+         sqlEx.printStackTrace(System.err);
+      } finally {
+         try {
+            if (results != null) {
+               results.close();
+               results = null;
+            }
+
+            if (statement != null) {
+               statement.close();
+               statement = null;
+            }
+         } catch (Exception ex) {
+            System.err.println("Error closing query: " + ex);
+            ex.printStackTrace(System.err);
+         }     
+      }
+
+      return teamScore;
+   }
+
+   public static double getPlayerScore(Connection conn, int week, String player) {
+      double score = qbScore(conn, week, player) + rbScore(conn, week, player) + recScore(conn, week, player);
       System.out.println(player + " on week " + week + " scored " + score + " points.");
 		return score;      
    }
 
-   public static int qbScore(Connection conn, int week, String player) {
+   public static double qbScore(Connection conn, int week, String player) {
    	Statement statement = null;
       ResultSet results = null;
-      int score = 0;
+      double score = 0;
+      String scoreStr = "";
 
-      String query = "SELECT SUM(yds) / 25 + X.tds AS pts FROM Play P JOIN Game G ON G.gid = P.gid \n" +
-   							"JOIN Pass S ON P.pid = S.pid JOIN Player L ON L.player = S.psr JOIN (SELECT " +
-        						"COUNT(*) * 4 AS tds, L.player FROM Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON S.pid = P.pid " +
-         					"JOIN Player L ON S.psr = L.player JOIN Td T ON P.pid = T.pid WHERE wk = " + week + " 	AND P.type = 'PASS'" +
-         					"AND L.player = '" + player + "' ) X ON X.player = L.player WHERE wk = " + week + " AND P.type = 'PASS'" +
-   							"AND L.player = '" + player + "'";
+      String query = "SELECT SUM(yds) / 25 + X.tds AS pts FROM Play P JOIN Game G ON G.gid = P.gid JOIN " + 
+                     "Pass S ON P.pid = S.pid JOIN Player L ON L.player = S.psr JOIN (SELECT COUNT(*) * " + 
+                     "4 AS tds, L.player FROM Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON S.pid = " + 
+                     "P.pid JOIN Player L ON S.psr = L.player JOIN Td T ON P.pid = T.pid WHERE wk = " + week + 
+                     " AND P.type = 'PASS' AND L.player = '" + player + "') X ON X.player = L.player WHERE wk = " + 
+                     week + " AND P.type = 'PASS' AND L.player = '" + player + "'";
 
       try {
          // Get a statement from the connection
          statement = conn.createStatement();
- 			System.out.println("statements\n"+query);
          // Execute the query
          results = statement.executeQuery(query);
- 			System.out.println("results\n");
-         String scoreStr = results.getString(1);
-         score = Integer.parseInt(scoreStr);
+         if(results.next())
+            scoreStr = results.getString(1);
+         if (scoreStr == null)
+            scoreStr = "0";
+
+         score = Double.parseDouble(scoreStr);
 
       } catch (SQLException sqlEx) {
          System.err.println("Error doing query: " + sqlEx);
@@ -89,44 +170,27 @@ public class Scoring {
       return score;
    }
 
-   /*public static int rbScore(Connection conn, int week, String player) {
+   public static double rbScore(Connection conn, int week, String player) {
    	Statement statement = null;
       ResultSet results = null;
-      int score;
+      double score = 0;
+      String scoreStr = "";
 
-      String query = "SELECT
-   							(SUM(R.yds) / 10) + X.tds AS pts
-							FROM
-   							Play P 
-   							JOIN Game G ON G.gid = P.gid
-   							JOIN Rush R ON R.pid = P.pid
-   							JOIN Player L ON R.bc = L.player
-   						JOIN (
-      						SELECT	
-         						COUNT(*) * 6 AS tds,
-         						L.player AS player
-      						FROM
-         						Play P
-         						JOIN Game G ON G.gid = P.gid
-	         					JOIN Rush R ON R.pid = P.pid
-   	      					JOIN Player L ON R.bc = L.player
-      	   					JOIN Td T ON P.pid = T.pid
-      						WHERE
-         						wk = " + week + " 
-         						AND P.type = 'RUSH'
-         						AND L.player = '" + player + "' 
-   						) X ON X.player = L.player
-							WHERE
-   							wk = " + week + " 
-   							AND P.type = 'RUSH'
-   							AND L.player = '" + player + "'";
+      String query = "SELECT (SUM(R.yds) / 10) + X.tds AS pts FROM Play P JOIN Game G ON G.gid = P.gid " + 
+   						"JOIN Rush R ON R.pid = P.pid JOIN Player L ON R.bc = L.player JOIN (SELECT COUNT(*) * 6 AS tds, " +
+         				"L.player AS player FROM Play P JOIN Game G ON G.gid = P.gid JOIN Rush R ON R.pid = P.pid JOIN Player L ON R.bc = L.player " +
+      	   			"JOIN Td T ON P.pid = T.pid WHERE wk = " + week + " AND P.type = 'RUSH' AND L.player = '" + player + "') X ON X.player = L.player " +
+							"WHERE wk = " + week + " AND P.type = 'RUSH' AND L.player = '" + player + "'";
       try {
          // Get a statement from the connection
          statement = conn.createStatement();
          // Execute the query
          results = statement.executeQuery(query);
-         String scoreStr = results.getString(1);
-         score = Integer.parseInt(scoreStr);
+         if(results.next())
+            scoreStr = results.getString(1);
+         if (scoreStr == null)
+            scoreStr = "0";
+         score = Double.parseDouble(scoreStr);
          
       } catch (SQLException sqlEx) {
          System.err.println("Error doing query: " + sqlEx);
@@ -150,44 +214,27 @@ public class Scoring {
       return score;
    }
 
-   public static int recScore(Connection conn, int week, String player) {
+   public static double recScore(Connection conn, int week, String player) {
    	Statement statement = null;
       ResultSet results = null;
-      int score;
+      double score = 0;
+      String scoreStr = "";
 
-      String query = "SELECT 
-   							(SUM(S.yds) / 10) + X.tds AS pts
-							FROM
-   							Play P
-   							JOIN Game G ON G.gid = P.gid
-   							JOIN Pass S ON P.pid = S.pid
-   							JOIN Player L ON S.trg = L.player
-   							JOIN (
-      							SELECT
-         							COUNT(*) * 6 AS tds,
-         							L.player
-      							FROM
-         							Play P
-         							JOIN Game G ON G.gid = P.gid
-         							JOIN Pass S ON S.pid = P.pid
-         							JOIN Player L ON S.trg = L.player
-         							JOIN Td T ON P.pid = T.pid
-      							WHERE
-         							wk = " + week + " 
-         							AND P.type = 'PASS'
-         							AND L.player = '" + player + "' 
-   							) X ON X.player = L.player 
-							WHERE
-   							wk = " + week + " 
-   							AND P.type = 'PASS'
-   							AND L.player = '" + player + "'";
+      String query = "SELECT (SUM(S.yds) / 10) + X.tds AS pts FROM Play P JOIN Game G ON G.gid = P.gid " +
+   						"JOIN Pass S ON P.pid = S.pid JOIN Player L ON S.trg = L.player JOIN (SELECT COUNT(*) * 6 AS tds, " +
+         				"L.player FROM Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON S.pid = P.pid JOIN Player L ON S.trg = L.player " +
+         				"JOIN Td T ON P.pid = T.pid WHERE wk = " + week + " AND P.type = 'PASS' AND L.player = '" + player + "') X ON X.player = L.player " + 
+							"WHERE wk = " + week + " AND P.type = 'PASS' AND L.player = '" + player + "'";
       try {
          // Get a statement from the connection
          statement = conn.createStatement();
          // Execute the query
          results = statement.executeQuery(query);
-         String scoreStr = results.getString(1);
-         score = Integer.parseInt(scoreStr);
+         if(results.next())
+            scoreStr = results.getString(1);
+         if (scoreStr == null)
+            scoreStr = "0";
+         score = Double.parseDouble(scoreStr);
          
       } catch (SQLException sqlEx) {
          System.err.println("Error doing query: " + sqlEx);
@@ -209,5 +256,5 @@ public class Scoring {
          }     
       }
       return score;
-   }*/
+   }
 }

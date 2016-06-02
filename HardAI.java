86b44f58ded535324f757b9ea5e49cb8c352586c
Gurjeevan Bains <gurjeevanb@gmail.com>
2016-06-01 18:55:42 -0700

    hard ai should be working

--- HardAI.java
+++ HardAI.java
@@ -7,6 +7,23 @@ import java.sql.Statement;
 import java.lang.*;
 
 public class HardAI {
+   /*public static void main(String[] args) throws Exception{
+      Connection conn = DBConnection.getConnection();
+      DBConnection.initTeam(conn, "bitchnigga");
+      draftPlayer(conn, 1, 3);
+      draftPlayer(conn, 2, 3);
+      draftPlayer(conn, 3, 3);
+      draftPlayer(conn, 4, 3);
+      draftPlayer(conn, 5, 3);
+      draftPlayer(conn, 6, 3);
+      draftPlayer(conn, 7, 3);
+
+      DBConnection.close(conn);
+
+
+
+   }*/
+
    public static void draftPlayer(Connection conn, int round, int teamId) {
 
       switch(round) {
@@ -114,19 +131,19 @@ public class HardAI {
    private static void selectPlayer(Connection conn, String pos, int teamId) {
       Statement statement = null;
       ResultSet results = null;
-      String playerPos = "";
       String playerStr = "";
+      String query = "";
 
       if(pos == "WR1" || pos == "WR2" || pos == "WR3")
-         playerPos = "WR";
+         query = wrReturn();
       else if(pos == "RB1" || pos == "RB2")
-         playerPos = "RB";
+         query = rbReturn();
       else if(pos == "QB")
-         playerPos = "QB";
+         query = qbReturn();
       else if(pos == "TE")
-         playerPos = "TE";
-      
-      String query = "SELECT player FROM Player WHERE dcp = 1 AND pos1 = '" + playerPos + "' AND teamOn = 0 ORDER BY RAND() LIMIT 1";
+         query = teReturn();
+
+
       try {
          statement = conn.createStatement();
          results = statement.executeQuery(query);
@@ -154,9 +171,54 @@ public class HardAI {
       DBConnection.updateTeam(conn, pos, playerStr, teamId);
    }
 
-
-   private static String bestSelection(Connection conn, String pos, int teamId) {
-   return"";
+   private static String qbReturn() {
+      return "SELECT Z.player FROM (SELECT AVG(Y.pts) AS avgPts, Y.player, Y.fname, Y.lname FROM (SELECT G.wk, " +
+             "SUM(yds) / 25 + COALESCE(X.tds, 0) AS pts, L.player, L.fname, L.lname, L.pos1, L.teamOn FROM " +
+             "Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON P.pid = S.pid JOIN Player L ON L.player = S.psr " +
+             "LEFT JOIN (SELECT wk, COUNT(*) * 4 AS tds, L.player FROM Play P JOIN Game G ON G.gid = P.gid " +
+             "JOIN Pass S ON S.pid = P.pid JOIN Player L ON S.psr = L.player JOIN Td T ON P.pid = T.pid WHERE " +
+             "P.type = 'PASS' AND teamOn = 0 GROUP BY wk, L.player) X ON X.player = L.player AND X.wk = G.wk " +
+             "WHERE P.type = 'PASS' AND teamOn = 0 GROUP BY G.wk, L.player ORDER BY L.player, G.wk) Y " +
+             "WHERE Y.pos1 = 'QB' AND Y.teamOn = 0 GROUP BY Y.player ORDER BY avgPts DESC LIMIT 10) Z " +
+             "ORDER BY RAND() LIMIT 1";
    }
+
+   private static String rbReturn() {
+      return "SELECT Z.player FROM (SELECT AVG(Y.pts) AS avgPts, Y.player, Y.fname, Y.lname FROM (SELECT G.wk, " + 
+             "(SUM(R.yds) / 10) + COALESCE(X.tds, 0) AS pts, L.player, L.fname, L.lname, L.pos1, L.teamOn FROM " +
+             "Play P JOIN Game G ON G.gid = P.gid JOIN Rush R ON P.pid = R.pid JOIN Player L ON R.bc = L.player " +
+             "LEFT JOIN (SELECT COUNT(*) * 6 AS tds, L.player, wk FROM Play P JOIN Game G ON G.gid = P.gid " +
+             "JOIN Rush R ON R.pid = P.pid JOIN Player L ON R.bc = L.player JOIN Td T ON P.pid = T.pid " +
+             "WHERE P.type = 'RUSH' AND teamOn = 0 GROUP BY wk, L.player) X ON X.player = L.player AND X.wk = G.wk " +
+             "WHERE P.type = 'RUSH' AND teamOn = 0 GROUP BY G.wk, L.player ORDER BY L.player, G.wk) Y " +
+             "WHERE Y.pos1 = 'RB' AND Y.teamOn = 0 GROUP BY Y.player ORDER BY avgPts DESC LIMIT 10) Z " +
+             "ORDER BY RAND() LIMIT 1";
+   }
+   
+   private static String wrReturn() {
+      return "SELECT Z.player FROM (SELECT AVG(Y.pts) AS avgPts, Y.player, Y.fname, Y.lname FROM (SELECT G.wk, " +
+             "(SUM(S.yds) / 10) + COALESCE(X.tds, 0) AS pts, L.player, L.fname, L.lname, L.pos1, L.teamOn FROM Play P " +
+             "JOIN Game G ON G.gid = P.gid JOIN Pass S ON P.pid = S.pid JOIN Player L ON S.trg = L.player LEFT JOIN (" +
+             "SELECT COUNT(*) * 6 AS tds, L.player, wk FROM Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON S.pid = P.pid " +
+             "JOIN Player L ON S.trg = L.player JOIN Td T ON P.pid = T.pid WHERE P.type = 'PASS' AND teamOn = 0 " +
+             "GROUP BY wk, L.player) X ON X.player = L.player AND X.wk = G.wk WHERE P.type = 'PASS' AND teamOn = 0 " +
+             "GROUP BY G.wk, L.player) Y WHERE Y.pos1 = 'WR' AND Y.teamOn = 0 GROUP BY Y.player ORDER BY avgPts DESC " +
+             "LIMIT 10) Z ORDER BY RAND() LIMIT 1";
+   }
+
+   private static String teReturn() {
+      return "SELECT Z.player FROM (SELECT AVG(Y.pts) AS avgPts, Y.player, Y.fname, Y.lname FROM (SELECT G.wk, " +
+             "(SUM(S.yds) / 10) + COALESCE(X.tds, 0) AS pts, L.player, L.fname, L.lname, L.pos1, L.teamOn FROM " +
+             "Play P JOIN Game G ON G.gid = P.gid JOIN Pass S ON P.pid = S.pid JOIN Player L ON S.trg = L.player " +
+             "LEFT JOIN (SELECT COUNT(*) * 6 AS tds, L.player, wk FROM Play P JOIN Game G ON G.gid = P.gid " +
+             "JOIN Pass S ON S.pid = P.pid JOIN Player L ON S.trg = L.player JOIN Td T ON P.pid = T.pid WHERE " +
+             "P.type = 'PASS' AND teamOn = 0 GROUP BY wk, L.player) X ON X.player = L.player AND X.wk = G.wk " +
+             "WHERE P.type = 'PASS' AND teamOn = 0 GROUP BY G.wk, L.player) Y WHERE Y.pos1 = 'TE' AND Y.teamOn = 0 " +
+             "GROUP BY Y.player ORDER BY avgPts DESC LIMIT 10) Z ORDER BY RAND() LIMIT 1";
+   }
+
+
+
+
 }
 

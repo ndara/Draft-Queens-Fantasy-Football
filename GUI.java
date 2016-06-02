5ac41e8052907eb5034a5b70ea4f6f238cdee9b8
lfvert <lfverast@calpoly.edu>
2016-06-02 15:43:11 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -707,7 +707,7 @@ public void winner()
 		next.addActionListener(ButtonListener);
 		JPanel but=new JPanel();
 		but.setLayout(new FlowLayout());
-		but.add(new JLabel("Eliminated: "+loser.substring(1));
+		but.add(new JLabel("Eliminated: "+loser.substring(1)));
 		//adds proper boolean to screen
 		
 		but.add(next);
@@ -891,4 +891,11 @@ public void winner()
 			}
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
 }

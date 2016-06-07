16ee0c97b1125e9677061da97d497395e2462b21
lfvert <lfverast@calpoly.edu>
2016-06-07 15:46:57 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -203,8 +203,6 @@ public void initDraft()
 	cTable.setEnabled(false);
      hTab.add(cTable);
 	 }
-     
-     
      tabs.addTab(teamName.getText(), hTab);
      //AI starts at 2
      //AI one
@@ -224,7 +222,7 @@ public void initDraft()
 	 a1Table.setValueAt("College",0,6);
 	 a1Table.setValueAt("NFL Team",0,7);
 	 
-     for(int x=0;x<tempp.size()-1;x++)
+     for(int x=0;x<tempp.size();x++)
 	{ 
 		String [] values=tempp.get(x).split("\\|");
 		System.out.println(values.length);
